f16f19ae43bc8e00fa79e4b8c1f84c6e5c07885f
petra4623 <74016638+petra4623@users.noreply.github.com>
2021-05-17 11:37:40 +0900

    이실환
    돈 닷지 미

---
+++ HelloUnity/Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float spawnRateMin = 0.5f;
+    public float spawnRateMax = 0.5f;
+
+    private Transform target;
+    private float spawnRate;
+    private float timerAfterSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerAfterSpawn = 0f;
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        target = FindObjectOfType<PlayerController>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerAfterSpawn += Time.deltaTime;
+
+        if(timerAfterSpawn >= spawnRate)
+        {
+            timerAfterSpawn = 0;
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            bullet.transform.LookAt(target);
+
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+                            
+
+        }
+    }
+}
